50ec6083fba166f8b4a31b247f035074f69536b8
locphan201 <u1275898@utah.edu>
2023-06-26 19:01:08 +0700

    Add Python course

---
+++ 3/script.js
@@ -0,0 +1,243 @@
+const videos = {
+  "Setup" : {
+    "0" : {
+      title : "Introduction",
+      url : "1Ki9dfiekoRxvLspM6ad-7Fb3CsZG4WPD"
+    },
+    "1" : {
+      title : "Install Python & Navigate Terminal",
+      url : "1eI5c9_8LV1ZhR2NKKYNFsGRoMqGzLLj0"
+    },
+    "2" : {
+      title : "Install Sublime Text",
+      url : "1t3M4Jb7UFBv-VdTqKQtYkmLTxyuodbun"
+    },
+    "3" : {
+      title : "Sublime Text Basic",
+      url : "1JdrYy7VBsp6EukPDZR_DhfnhdhlpXZ0B"
+    },
+    "4" : {
+      title : "How To Run Python Files",
+      url : "1SRIHQOEdszaykiE4sO-hR4LRd9hcFT3L"
+    },
+    "5" : {
+      title : "PIP Installation",
+      url : "1T8jPd0tcXzNy9OxXwjOmZQ-D___2yBl6"
+    }
+  },
+  "If-Else & Datatypes" : {
+    "0" : {
+      title : "Datatypes & Variables",
+      url : "1_s7r9rnR6i5W1h-jielMXw6OcIJtwUoq"
+    },
+    "1" : {
+      title : "Inputs & Variables",
+      url : "1jycVK1o76LDb3Eg2D3EJY3V_-3-I_T-G"
+    },
+    "2" : {
+      title : "String & Float",
+      url : "1ARuQodotlGbYBS4GGp8_T0iISOEHCQZ2"
+    },
+    "3" : {
+      title : "If-Else",
+      url : "1vO6J_anbuiQyXGAmZMNCP0wfUejwIrrq"
+    },
+    "4" : {
+      title : "Boolean",
+      url : "149hbrxuMcang2Us6Io15_TFnZE_Wwfm0"
+    },
+    "5" : {
+      title : "AND & OR (Logic Algebra)",
+      url : "1EReS5pt3xOyj8jRCY4e2BbnYdcuuAl2y"
+    },
+    "6" : {
+      title : "Boolean & None Value",
+      url : "1Sgjxm1ln5wsdAyrO_rInkB_ziySxow-B"
+    },
+    "7" : {
+      title : "AND & OR & If-Else & None",
+      url : "1u2M0d2IeVS6OxnM0IqXyPZTu0l8g7iMb"
+    },
+  },
+  "Loops" : {
+    "0" : {
+      title : "For Loop",
+      url : "1UJH0QBzCRHhWQqq0_MM2hGcxGuCqi66s"
+    },
+    "1" : {
+      title : "Nested For Loop",
+      url : "1zYTEaw6ZUBwPiTOtEhpBNqWx-UnZzkid"
+    },
+    "2" : {
+      title : "Break & Continue",
+      url : "1LVWIqpAvWT58l1OP7SCV0UEmQJxYQnrO"
+    },
+    "3" : {
+      title : "Practise For Loop",
+      url : "1EDomZxDd1BZ34UQll66pQ17OSXjRfFQJ"
+    },
+    "4" : {
+      title : "While Loop",
+      url : "1bXLYqzFD3rv4j2Wvb4fHURAotdtT-K1t"
+    },
+    "5" : {
+      title : "Fix If-Else Bugs",
+      url : "1gasYsrdWmewyBAZNu4PIBHmTl-7JMq-l"
+    },
+    "6" : {
+      title : "Practise Using Loop",
+      url : "1xZP4bpRq_2tER1r720eZaZNTJjuFoVK9"
+    },
+    "7" : {
+      title : "While True & Break",
+      url : "1ByrmGqZtNt9wksnry21KBLIy6frEba1X"
+    },
+  },
+  "Functions" : {
+    "0" : {
+      title : "Procedure & Input & Output",
+      url : "1cdh3E6xVI1OWZXhAgRZBVHmGBiUHZLbI"
+    },
+    "1" : {
+      title : "Parameter & Argument",
+      url : "1CCgfNqoY2VnaDI77kRb9Bpr7pvdMcJV4"
+    },
+    "2" : {
+      title : "Multiple Parameters",
+      url : "18Z5XaWxCbXVT2hDoJohwBy5S9nV8ABXV"
+    },
+    "3" : {
+      title : "Return Value",
+      url : "183r4RM-LgfvOPFf-pSGxgpzZN253bgg9"
+    },
+    "4" : {
+      title : "Main Function",
+      url : "1vhq4vKk8lobWygMbeudPzwhB2Jhchjxz"
+    },
+    "5" : {
+      title : "Scope & Local & Global",
+      url : "1QpMPAJFy3KIBjq3C36xUXtxpZtHF5a9D"
+    },
+    "6" : {
+      title : "Practise Using Functions",
+      url : "1lNaJG5Hl5Yqx6X49E3G8qxhbC4Zn-IUc"
+    }
+  },
+  "Simple Project using Functions" : {
+    "0" : {
+      title : "Overview - Cashier Program",
+      url : "1wlK8STJRgfZGmQVABRY5yNxTK4ifoGf0"
+    },
+    "1" : {
+      title : "Pseudocode",
+      url : "1105BW2Famo-lO9QzqcQ_ML9b8gQyuyA9"
+    },
+    "2" : {
+      title : "Calculate Total Price & Return Money",
+      url : "189ipSMxZWwCXi3DOs7ZEZd1pVzpmRq8W"
+    },
+    "3" : {
+      title : "Calculate Return Cash",
+      url : "1WUsOwInlqPB-tPCU8sRrgLCeshaKi69U"
+    },
+  },
+  "Refactoring with Functions" : {
+    "0" : {
+      title : "Yes/No Function",
+      url : "1CJasjZuRJjBePByXQ9KXsPyjyhYIuNTe"
+    },
+    "1" : {
+      title : "Calculate Age Function",
+      url : "1YdPcdWMA7iXyU4uTjsJMWPws1hd_AmAs"
+    },
+    "2" : {
+      title : "Convert Function",
+      url : "1U3_XfmjPR99FVIjfYfQE7dF4upH9p70k"
+    },
+    "3" : {
+      title : "Print Info Function",
+      url : "15VYLM_27Vke0hd1RKGUH8oDKUFlSC0vw"
+    }
+  },
+  "File I/O & List" : {
+    "0" : {
+      title : "Write Text File",
+      url : "1xMOQtTL_RiWhnwezEj6xYJYxL0IeL5Df"
+    },
+    "1" : {
+      title : "Read Text File",
+      url : "1R82p4ylayXu7QXjaBeIYPEazGvSg6QGQ"
+    },
+    "2" : {
+      title : "List",
+      url : "1yo7zSGQzOHdFEAyV12WuAIk2NN1CTmPS"
+    },
+    "3" : {
+      title : "List - Basic Operations",
+      url : "1zSSX2lSByEQT3ZQlmB0jEdgwrzHbYhOp"
+    },
+    "4" : {
+      title : "Practise Using List & File I/O",
+      url : "1DuT_qSE6TtsldJB9p_aBiTvT_Cs6gYcN"
+    }
+  },
+  "Class" : {
+    "0" : {
+      title : "Class",
+      url : "1KafJ-8o7Xbxli_QJUMOoXZIlBWQCEmt3"
+    }
+  },
+}
+
+libraries = ["Requests", "Django", "Flask", "BeautifulSoup", "Selenium",
+             "NumPy", "Pandas", "Matplotlib", "OpenCV", "PtQt5", "Pygame"]
+
+const keys = Object.keys(videos)
+
+const container = document.getElementById('container')
+var content = ''
+var navContent = '<div class="module"><h3 class="module-title">Sections</h3><ul class="module-content">'
+
+for (var i = 0; i < keys.length; i++) {
+  navContent += `<li><a href="#${i}">${i} - ${keys[i]}</a></li>`
+
+  content += `
+  <div id="${i}" class="module">
+    <h3 class="module-title">${keys[i]}</h3>
+      <ul class="module-content">`
+
+  const lessons = Object.values(videos[keys[i]])
+  for (var j = 0; j < lessons.length; j++) {
+    content += `<li><a onclick="changeVideo(${i}, ${j})">Lesson ${j+1}: ${lessons[j].title}</a></li>`
+  }
+  content += '</ul></div>'
+}
+
+content += '<h3>Useful Libraries in Python</h3>'
+content += `
+  <div class="module">
+    <h3 class="module-title">Resources</h3>
+      <ul class="module-content">`
+
+for (var i = 0; i < libraries.length; i++) {
+  content += `<li><a>${libraries[i]}</a></li>`
+}
+content += '</ul></div>'
+
+container.innerHTML += navContent + '</ul></div>' + content
+
+const videoTitle = document.getElementById('video-title')
+const videoFrame = document.getElementById('video') 
+
+function changeVideo(sect, lesson) {
+  const videoInfo = Object.values(videos[keys[sect]])[lesson]
+  videoTitle.textContent = videoInfo.title
+  videoFrame.src = `https://drive.google.com/file/d/${videoInfo.url}/preview`
+  
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+}
+
+changeVideo(0, 0)
